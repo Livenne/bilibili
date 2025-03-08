1268bbaec7f41380d6347ce3caf60571c79ab6a8
Livenne <2133249839@qq.com>
2025-03-08 19:49:13 +0800

    Rewrite

--- src/main/java/com/livenne/bilibili/Repository/VideoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.livenne.bilibili.Repository;
-
-import com.livenne.bilibili.Pojo.Video;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VideoRepository extends CrudRepository<Video, Long> {
-}
